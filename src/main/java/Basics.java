9c10ebf65686ae860774acd3b2d2276213f4ab66
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-09-26 16:47:01 -0400

    01 Intro to Java Exercise Completed

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -41,9 +41,6 @@ public class Basics {
         System.out.println(7 + 5);
 
         System.out.println("Hello World!");
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
 
 
 
@@ -60,11 +57,6 @@ public class Basics {
          */
 
         int my_variable = 100;
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
-
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -93,9 +85,7 @@ public class Basics {
             System.out.println("Current count: " + i);
         }
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
+
          *
          * e.g. the end result should look like:
          * Current count: 10
@@ -138,16 +128,15 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
-         *                The String methods .split and .charAt may be helpful,
-         *                along with the StringBuilder.append
-         *                You may also assume that to_split always has 7 words
-         *                separated by spaces, so no loops are required, though
-         *                you may use them if you wish.
+
+        var words = to_split.split(" ");
+        /*
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        for (int j=0; j < 7; j ++) {
+            ret.append(words[j].charAt(0));
+        }
 
         return ret.toString();
     }
@@ -169,11 +158,17 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
+        if (arr.length < 2) {
+            return 0;
+        } else {
+            for (int i=0; i < arr.length; i++) {
+                if (i%2 == 1) {
+                    current_sum += arr[i];
+                }
+            }
+        }
+
+        /*
          */
 
         return current_sum;
